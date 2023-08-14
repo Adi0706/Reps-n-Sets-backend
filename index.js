c9087cf71d7f5b027443fe38e6128d25fd9b3365
Aditya Bhattacharjee <adityab76@gmail.com>
2023-08-14 14:48:37 +0530

    login backend debug and updated

--- index.js
+++ index.js
@@ -1,55 +1,54 @@
-const express = require ('express')
-const mongoose = require('mongoose')
-const cors = require ('cors')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 const SignupModel = require('./models/signup');
-const LoginModel = require('./models/login');
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
+mongoose.connect('mongodb://127.0.0.1:27017/repsNsets', { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connect('mongodb://127.0.0.1:27017/repsNsets')
-
-
-
-
-
-app.post('/login',(req,res)=>{
-    const {loginEmail,loginPassword}=req.body ; 
-    LoginModel.findOne({email:loginEmail})
-    .then(user=>{
-        if(user){
-            if(user.password === loginPassword){
-                res.json("Login Successfull")
-            }else{
-                res.json("Incorrect Password")
-            }
-        }else{
-            res.json("Account not found")
+app.post('/login', async (req, res) => {
+    const { loginEmail, loginPassword } = req.body;
+    try {
+      const user = await SignupModel.findOne({ email: loginEmail });
+      if (user) {
+        if (user.password === loginPassword) {
+          res.json("Login Successful");
+        } else {
+          res.json("Incorrect Password");
         }
-    })
+      } else {
+        res.json("Account not found");
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).json("Error fetching user data");
+    }
+});
 
-})
+app.post('/signups', async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const existingUser = await SignupModel.findOne({ email: email });
+    if (existingUser) {
+      res.json("Already have an Account");
+    } else {
+      await SignupModel.create({
+        name: name,
+        email: email,
+        password: password
+      });
+      res.json("Account Created");
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json("Error");
+  }
+});
 
-
-
-
-app.post('/signups',(req,res)=>{
-    const {name,email,password}=req.body;
-    SignupModel.findOne({email:email}).then(user=>{
-        if(user){
-            res.json("Already have an Account")
-        }else{
-            SignupModel.create({
-                name:name,email:email,password:password
-            }).then(res=>res.json("Account Created"))
-            .catch(err=>res.json("Error"))
-        }
-    }).catch(err=>res.json(err))
-    
-})
-
-app.listen(3002,()=>{
-    console.log("Server is Running")
-})
+const PORT = 3002;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
